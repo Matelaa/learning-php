cf70aa9af942bd276403d23f7512e9242f426009
Matelaa <josematelaneto@edu.unifor.br>
2020-03-09 23:43:12 -0300

    strings module finished

---
+++ strings/strings.php
@@ -0,0 +1,44 @@
+<?php
+
+    $stringOne = 'my email is ';
+    $stringTwo = 'jmatelaneto@gmail.com';
+
+    // Concatenation
+//    echo $stringOne . $stringTwo;
+
+    $name = 'Matela';
+//    echo 'Hey, my name is ' . $name;
+
+    // The difference between `''` and `""` is that `""` can concatenate within it
+//    echo "Hey, my name is $name";
+
+    // This will return exactly what is written
+//    echo 'Hey, my name is $name';
+
+//    echo "The ninja screamed \"whaaaaa\"";
+//    echo 'The ninja screamed "whaaaaa"';
+
+    // Get especifc character of String
+//    echo $name[0];
+
+    // String functions
+//    echo strlen($name);
+//    echo strtoupper($name);
+//    echo strtolower($name);
+//    echo str_replace('M', 'W', $name);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Strings</title>
+</head>
+<body>
+
+</body>
+</html>
