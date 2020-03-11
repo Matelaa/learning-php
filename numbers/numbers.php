87a72c58bfcb76fbe54febcfba867deb668ff226
Matelaa <josematelaneto@edu.unifor.br>
2020-03-11 00:12:40 -0300

    numbers module finished

---
+++ numbers/numbers.php
@@ -0,0 +1,59 @@
+<?php
+
+    // Integer
+    $radius = '25';
+
+    // Float
+    $pi = 3.14;
+
+    // Basic Operators ( - | * | + | / | ** )
+
+//    echo $pi * $radius**2;
+
+    // Order of Operation ( B (BRACKETS) I (INDICES) D (DIVISION) M (MULTIPLICATION) A (ADDITION) S (SUBTRACTION) )
+
+//    echo 2 * (4 + 9) / 3;
+
+    // Increment & Decrement Operators
+
+//    $radius++;
+//    echo $radius;
+//
+//    $radius--;
+//    echo $radius;
+
+    // Shorthand Operators
+    $age = 20;
+
+//    $age += 10;
+//    echo $age;
+
+//    $age -= 10;
+//    echo $age;
+
+//    $age *= 2;
+//    echo $age;
+
+    // Number Functions
+
+    // round a number to the nearest integer (down)
+//    echo floor($pi);
+    // round a number to the nearest integer (up)
+//    echo ceil($pi);
+//    echo pi();
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Numbers</title>
+</head>
+<body>
+
+</body>
+</html>
