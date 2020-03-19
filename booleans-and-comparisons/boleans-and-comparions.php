8fef87f99bb88e0afaee0d81325f6a7730f174cb
Matelaa <josematelaneto@edu.unifor.br>
2020-03-19 16:49:54 -0300

    booleans and comparisons module finished

---
+++ booleans-and-comparisons/boleans-and-comparions.php
@@ -0,0 +1,65 @@
+<?php
+
+//    $products = [
+//        ['name' => 'Shiny Star', 'price' => 20],
+//        ['name' => 'Green Shell', 'price' => 10],
+//        ['name' => 'Red Shell', 'price' => 15],
+//        ['name' => 'Gold Coin', 'price' => 5],
+//        ['name' => 'Lightning Bolt', 'price' => 40],
+//        ['name' => 'Banana Skin', 'price' => 2]
+//    ];
+//
+//    $i = 0;
+//    while ($i < count($products)) {
+//        echo $products[$i]['name'] . '<br />';
+//        $i++;
+//    }
+
+    // Comparisons Booleans (true or false)
+
+    // true return an 1
+//    echo true;
+
+    // false return an empty string
+//    echo false;
+
+    // Numbers
+//    echo 5 < 10;
+//    echo 5 > 10;
+//    echo 5 == 10;
+//    echo 10 == 10;
+//    echo 5 != 10;
+//    echo 5 <= 5;
+//    echo 5 >= 5;
+
+    // Strings
+//    echo 'Jose' < 'Matela';
+//    echo 'Jose' > 'Matela';
+//    echo 'jose' > 'Jose';
+//    echo 'Matela' == 'Matela';
+//    echo 'matela' == 'Matela';
+
+    // Loose vc Strict equal comparions
+//    echo 5 == '5';
+//    echo 5 === '5';
+//    echo 5 === 5;
+
+//    echo true == '1';
+//    echo false == '';
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Booleans and Comparisons</title>
+</head>
+<body>
+
+</body>
+</html>
