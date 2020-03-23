687bd1b97dc25d95b8cbf3ba545fbce92e8a8457
Matelaa <josematelaneto@edu.unifor.br>
2020-03-23 00:30:57 -0300

    include and require module finished

---
+++ include-and-require/include-and-require.php
@@ -0,0 +1,27 @@
+<?php
+
+//    include('persons.php');
+//    require('persons.php');
+
+//    require 'persons.php'; -> fatal error, dont reproduce the rest of the code
+//    include 'persons.php'; -> warning, reproduce the rest of the code
+
+    echo 'end of php';
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Include & Require</title>
+</head>
+<body>
+    <?php include ('content.php') ?>
+    <?php include ('content.php') ?>
+    <?php include ('content.php') ?>
+</body>
+</html>
---
+++ include-and-require/persons.php
@@ -0,0 +1,6 @@
+<?php
+
+    $persons = ['Jose', 'Matela', 'Neto'];
+    echo "{$persons[1]} <br />";
+
+?>
